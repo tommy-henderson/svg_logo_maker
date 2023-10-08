442b6ac02b63c4a2bdd31d8712314e3ff5616136
Tommy Henderson <tommyhenderson@Tommys-MBP.ht.home>
2023-10-08 15:45:05 -0500

    troubleshooting errors in index.js

--- index.js
+++ index.js
@@ -27,29 +27,22 @@ function promptUser() {
     }
   ])
     .then((answers) => {
-      // Error handling for text prompt (user must enter 3 characters or less for logo to generate)
-      if (answers.text.length > 3) {
+      if (answers.initials.length > 3) {
         console.log("Must enter a value of no more than 3 characters");
         promptUser();
       } else {
-        // Calling write file function to generate SVG file
         writeToFile("logo.svg", answers);
       }
     });
 }
 
 function writeToFile(fileName, answers) {
-  // File starts as an empty string
   let svgData = "";
-  // Sets width and height of logo container
   svgData =
     '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
-  // <g> tag wraps <text> tag so that user font input layers on top of polygon -> not behind
   svgData += "<g>";
-  // Takes user input for shape choice and inserts it into SVG file
   svgData += `${answers.shape}`;
 
-  // Conditional check takes users input from choices array and then adds polygon properties and shape color to SVG string
   let shapeChoice;
   if (answers.shape === "Triangle") {
     shapeChoice = new Triangle();
@@ -62,14 +55,10 @@ function writeToFile(fileName, answers) {
     svgData += `<circle cx="150" cy="115" r="80" fill="${answers.shapeColor}"/>`;
   }
 
-  // <text> tag gives rise to text alignment, text-content/text-color taken in from user prompt and gives default font size of "40"
-  svgData += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
-  // Closing </g> tag
+  svgData += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.initials}</text>`;
   svgData += "</g>";
-  // Closing </svg> tag
   svgData += "</svg>";
 
-  // Using file system module to generate svg file, takes in file name given in the promptUser function, the svg string, and a ternary operator which handles logging any errors, or a "Generated logo.svg" message to the console  
   fs.writeFile(fileName, svgData, (err) => {
     err ? console.log(err) : console.log("Generated logo.svg");
   });
